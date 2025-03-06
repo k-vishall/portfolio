8268f519facfc13545d0578cfa225e03f230b042
Vishal Kumar <66443376+k-vishall@users.noreply.github.com>
2025-03-06 23:21:29 +0530

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/k-vishall.github.io/",
+  base: "/portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
